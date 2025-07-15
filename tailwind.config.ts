c0e16a686fc7a2330d145b24ea179f9970431546
Mr. NoBuddy <randomnobuddy@gmail.com>
2025-07-15 15:03:34 +0000

    create a folder of fonts where i will put edo.ttf
    and then kalam will b

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['PT Sans', 'sans-serif'],
-        headline: ['Kalam', 'cursive'],
+        headline: ['Edo', 'Kalam', 'cursive'],
         code: ['monospace'],
       },
       colors: {
